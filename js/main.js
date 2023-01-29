e259424ef6382272217aa38a1cd1bae5d2bb4882
ZvarycH23 <Zvarychxx23@gmail.com>
2023-01-29 20:04:05 +0200

    add adaptive final

--- js/main.js
+++ js/main.js
@@ -1,14 +1,41 @@
-  var owl = $('.owl-carousel');
-  owl.owlCarousel({
-    center:true,
-    loop:true,
-    margin:30 ,
-    startPosition:1
+// Carousel
+const owl = $('.owl-carousel');
+
+owl.owlCarousel({
+	center: true,
+	loop: true,
+	margin: 20,
+	startPosition: 0,
+	items: 1,
+	responsive: {
+		540: {
+			items: 3,
+			startPosition: 1,
+		},
+		1200: {
+			items: 3,
+			margin: 30,
+		},
+	},
 });
-  // Go to the next item
-  $('.slider_btn--next').click(function() {
-      owl.trigger('next.owl.carousel');
-  })
-  $('.slider_btn--prev').click(function() {
-      owl.trigger('prev.owl.carousel', [300]);
-  })
+
+$('.slider__btn--prev').click(function () {
+	owl.trigger('prev.owl.carousel');
+});
+
+
+$('.slider__btn--next').click(function () {
+	owl.trigger('next.owl.carousel');
+});
+
+// Nav icon
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
+const menuIcon = document.querySelector('.menu-icon');
+
+
+navBtn.onclick = function () {
+	nav.classList.toggle('nav--mobile')
+	menuIcon.classList.toggle('menu-icon--active');
+	document.body.classList.toggle('no-scroll');
+};
